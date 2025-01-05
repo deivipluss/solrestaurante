07311f07f58fe93ebde36d5a711099d0658b9dc4
Deivis Contreras Cardenas <deivipluss@gmail.com>
2025-01-05 23:59:42 +0000

    new1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Habilita el modo estricto de React para detectar errores más fácilmente
+  experimental: {
+    appDir: true, // Necesario para usar la nueva estructura de "app/"
+  },
 };
 
 export default nextConfig;
