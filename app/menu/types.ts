ca255c586350946c99810a5f1ce8cf890ffa1f99
Deivis Contreras Cardenas <deivipluss@gmail.com>
2025-01-20 21:48:23 +0000

    mod

--- app/menu/types.ts
+++ app/menu/types.ts
@@ -1,17 +1,23 @@
 export interface MenuItem {
-    name: string;
-    description?: string;
-    price: string;
-    image?: string;
-    popular?: boolean;
-  }
-  
-  export interface MenuSection {
-    title: string;
-    description?: string;
-    items: MenuItem[];
-  }
-  
-  export interface MenuCardProps {
-    item: MenuItem;
-  }
+  name: string
+  description?: string
+  price: string
+  image?: string
+  popular?: boolean
+}
+
+export interface MenuSection {
+  title: string
+  description?: string
+  items: MenuItem[]
+}
+
+export interface MenuCardProps {
+  item: MenuItem
+}
+
+export interface TestimonialProps {
+  text: string
+  author: string
+}
+
