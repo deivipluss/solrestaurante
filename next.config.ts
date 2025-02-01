c4d58acf3646d0c3064f3c19fb15774e733dc91b
Deivis Contreras Cardenas <deivipluss@gmail.com>
2025-02-01 23:10:29 +0000

    fixmodalREV6

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["placehold.co"], // Dominios permitidos para imágenes
+  },
+  // Opcional: Configuración adicional recomendada
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  // Internationalized Routing (opcional)
+  i18n: {
+    locales: ["es-PE"],
+    defaultLocale: "es-PE",
+  }
 };
 
 export default nextConfig;
